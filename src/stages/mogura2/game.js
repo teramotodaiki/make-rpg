fd4aacc2ddfeb7e26db646e1beed66db0452a46a
kassy_kz <kashimoto.kazutoshi@gmail.com>
2017-12-06 17:34:03 +0900

    mogura2追加

---
+++ src/stages/mogura2/game.js
@@ -0,0 +1,169 @@
+import 'hackforplay/core';
+import * as sequence from 'sequence';
+
+/* ここの部分は選手には見えません
+ * デバッグ中につき魔道書は最初から表示されています
+ */
+var mCoinScore = 1;
+var coinArray = new Array();
+var trapArray = new Array();
+var trapFlag = false;
+var startPlayerX = 1;
+var startPlayerY = 2;
+
+async function gameFunc() {
+	resetMap();
+
+	const player = self.player = new Player(); // プレイヤーをつくる
+	player.locate(startPlayerX, startPlayerY); // はじめの位置
+	player.on(('▼ イベント', 'こうげきするとき'), (event) => {
+		const 使い手 = event.target;
+		const ビーム = new RPGObject();
+		ビーム.mod(('▼ スキン', Hack.assets.energyBall));
+		ビーム.onふれはじめた = (event) => {
+			if (event.hit !== 使い手) {
+				Hack.Attack(event.mapX, event.mapY, 使い手.atk);
+				ビーム.destroy();
+			}
+		};
+		使い手.shoot(ビーム, 使い手.forward, 10);
+	});
+	/*+ スキル */
+
+	// さいしょの向きをかえる
+	player.forward = [1, 0];
+
+	// 詠唱待ち時間設定
+	window.WAIT_TIME = 3000;
+
+	// ゲーム時間設定
+	window.TIME_LIMIT = 300 * 1000;
+
+	// せつめい
+	const description = new enchant.Sprite(388, 224);
+	description.image = game.assets['resources/start_message_01'];
+	description.moveTo(46, 48);
+	Hack.menuGroup.addChild(description);
+
+	// const startButton = new enchant.Sprite(120, 32);
+	// startButton.image = game.assets['resources/start_button'];
+	// startButton.moveTo(180, 220);
+	// Hack.menuGroup.addChild(startButton);
+	// startButton.ontouchstart = () => {
+	// 	Hack.menuGroup.removeChild(description);
+	// 	Hack.menuGroup.removeChild(startButton);
+	// 	// タイマー開始
+	// 	Hack.startTimer();
+
+	// 	// 魔道書のコードをひらく
+	// 	feeles.openCode('stages/mogura/code.js');
+	// };
+
+	// 説明画面（作戦タイム）のタイマー => ゲームスタート
+	const strategyTimer = new enchant.ui.MutableText(352, 8);
+	const limit = Date.now() + window.STRATEGY_TIME;
+	strategyTimer.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+	strategyTimer.on('enterframe', () => {
+		const last = Math.max(0, limit - Date.now()) / 1000 >> 0;
+		strategyTimer.text = 'TIME:' + last;
+		if (last <= 0) {
+			Hack.menuGroup.removeChild(description);
+			// Hack.menuGroup.removeChild(startButton);
+			// タイマー開始
+			Hack.startTimer();
+		
+			// 魔道書のコードをひらく
+			feeles.openCode('stages/mogura/code.js');
+			
+			// 削除
+			Hack.menuGroup.removeChild(strategyTimer);
+		}
+	});
+	Hack.menuGroup.addChild(strategyTimer);
+
+	feeles.closeCode();
+	feeles.closeReadme();
+
+	Hack.on('gameclear', function () {
+		// 一旦削除
+		const score = Hack.score;
+		Hack.scoreLabel.score = 0;
+		Hack.menuGroup.removeChild(Hack.scoreLabel);
+		setTimeout(() => {
+			// スコアラベル表示
+			Hack.scoreLabel.moveBy(0, 210);
+			Hack.overlayGroup.addChild(Hack.scoreLabel);
+			Hack.scoreLabel.score = score;
+		}, 1000);
+
+		// 次へボタン
+		const nextButton = new enchant.Sprite(120, 32);
+		nextButton.image = game.assets['resources/next_button'];
+		nextButton.moveTo(180, 260);
+		nextButton.ontouchstart = () => {
+			// stage 1.5 へ
+			feeles.replace('stages/1/index.html');
+		};
+
+		setTimeout(() => {		
+			Hack.overlayGroup.addChild(nextButton);		
+		}, 4000);
+	});
+
+	feeles.setTimeout(timerFunc, 1000);
+	feeles.setAlias('getSafeTime', getSafeTime);
+
+}
+
+function resetMap() {
+	const map1 = Hack.createMap(`
+		10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|00 00 05 00 05 00 05 00 05 00 05 00 00 10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|00 00 05 00 05 00 05 00 05 00 05 00 00 10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|00 00 00 00 00 00 00 00 00 00 00 00 00 10|
+		10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|
+	`);
+	Hack.maps.map1 = map1;
+
+	Hack.changeMap('map1'); // map1 をロード		
+}
+
+var timerCount = 0;
+var moguraX = 0;
+var moguraY = 0;
+
+function timerFunc() {
+	moguraOn(moguraX + 3, moguraY + 3);
+	moguraX = (moguraX + 2) % 10;
+	moguraY = (moguraY + 3) % 6;
+}
+
+var itemMogura;
+
+function moguraOn(x, y) {
+	itemMogura = new RPGObject();
+	itemMogura.mod(('▼ スキン', _tつぼ));
+	itemMogura.locate(x, y, 'map1');
+	itemMogura.layer = RPGMap.Layer.Under;
+	itemMogura.onこうげきされた = () => {
+		moguraOff();
+		Hack.score += 10;
+		feeles.setTimeout(timerFunc, 1000);
+	};
+}
+function moguraOff() {
+	itemMogura.destroy();
+}
+
+Hack.onreset = function() {
+	resetMap();
+	player.locate(startPlayerX, startPlayerY); // はじめの位置
+	player.forward = [1, 0];
+};
+
+export default gameFunc;
