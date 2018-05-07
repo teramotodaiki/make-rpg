58876d0272848114dfad5de78a1a01144342c8db
teramotodaiki <i101323@gmail.com>
2018-05-07 14:29:45 +0900

    fix: CORS Error 対策のため feeles.fetchDataURL をつかう

--- src/raw-loader.js
+++ src/raw-loader.js
@@ -3,8 +3,14 @@
 // raw-loader: inspired https://github.com/webpack-contrib/raw-loader
 export async function load(mapName, localRequire, load) {
 	
-	const response = await feeles.fetch(mapName);
-	const text = await response.text();
+	// NOTICE: feeles.fetch は, base64 encode されたファイルの中身を
+	// さらに Blob にして Response に与えている
+	// この Blob をもとに Blob URL を生成したとき,
+	// Windows/Chrome では Cross Origin Request として扱われる
+	// これを回避するため, feeles.fetchDataURL を使う
+	const dataURL = await feeles.fetchDataURL(mapName);
+	const base64 = dataURL.split(',')[1];
+	const text = decodeURIComponent(escape(atob(base64)));
 	load(text);
 	
 }
